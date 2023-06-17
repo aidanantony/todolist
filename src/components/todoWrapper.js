42131471ee92ca031cf8ee45feab357e88ccf10c
Aidan Antony <aidanantony65@gmail.com>
2023-06-17 13:38:38 -0600

    Add ability to cross off tasks

--- src/components/todoWrapper.js
+++ src/components/todoWrapper.js
@@ -10,10 +10,18 @@ export const TodoWrapper = () => {
     setTodos([...todos, {id: uuidv4(), task: todo, compelted:false, isEditing: false}])
     console.log(todos)
   }
+  const toggleComplete = id => {
+    setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} :todo))
+  }
 return (
   <div className="TodoWrapper"> 
+    <h1>Get Things Done!</h1>
     <TodoForm addTodo={addTodo}/>
-    <Todo />
+    {todos.map((todo,index) => (
+        <Todo task={todo} key={index} 
+        toggleComplete={toggleComplete}
+        />
+    ))}
   </div>
   )
 }
