e93ab25f893fdd5666dca07f90d17dce1fed9786
Aidan Antony <aidanantony65@gmail.com>
2023-06-17 13:13:13 -0600

    Add todo box with icons

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({addTodo}) => {
+  const [value,setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addTodo(value)
+
+    setValue("")
+  }
 return (
-  <form className='TodoForm'> 
+  <form className='TodoForm' onSubmit={handleSubmit}> 
     <input type='text' className='todo-input' 
-    placeholder="What are the tasks for today?" />
+    value={value} 
+    placeholder="What are the tasks for today?" 
+    onChange={(e) => setValue(e.target.value)}/>
     <button type='submit' className='todo-btn'>
+      Add task
     </button>
   </form>
   )
--- src/components/todoWrapper.js
+++ src/components/todoWrapper.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoForm } from "./TodoForm"
+import { Todo } from "./Todo"
+import { v4 as uuidv4 } from 'uuid'
+uuidv4();
 
 export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([])
+  const addTodo = todo => {
+    setTodos([...todos, {id: uuidv4(), task: todo, compelted:false, isEditing: false}])
+    console.log(todos)
+  }
 return (
   <div className="TodoWrapper"> 
-    <TodoForm />
+    <TodoForm addTodo={addTodo}/>
+    <Todo />
   </div>
   )
 }
